ed39418f31e07c029acd2e39a1c95b3d923a5dff
djouaniahmed87-ctrl <djouaniahmed87@gmail.com>
2025-11-19 17:43:22 +0100

    Delete imeges/css directory

--- imeges/css/js/script.js
+++
@@ -1,9 +0,0 @@
-// مثال تفاعل: زر يظهر رسالة عند الضغط
-document.addEventListener("DOMContentLoaded", function() {
-    const buttons = document.querySelectorAll(".alert-button");
-    buttons.forEach(button => {
-        button.addEventListener("click", function() {
-            alert("تم الضغط على الزر!");
-        });
-    });
-});
